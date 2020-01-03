cda5e635cd8bf32df6a8be4ab9f2d05c28df388f
dydx <dydx2000.py@gmail.com>
2020-01-03 13:24:21 +0800

    add from 706,2020-01-13

---
+++ src/Day01/array01.java
@@ -0,0 +1,17 @@
+package Day01;
+
+import java.lang.reflect.Array;
+
+public class array01 {
+    public static void main(String[] args) {
+
+//        string[] int1 = {"a","b","b"};
+        int[] numarrary = {1,2,3,4,5};
+        for(int i=0;i<numarrary.length;i++){
+            System.out.print(numarrary[i]+" ");
+        }
+
+    }
+
+
+}
